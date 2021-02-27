9dfae8a5eb3c4474a4b20285a175706987844409
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2021-02-27 02:01:30 +0000

    fix: fix contact form submit issue

--- src/containers/ContactFormContainer.js
+++ src/containers/ContactFormContainer.js
@@ -6,16 +6,18 @@ export default function ContactFormContainer() {
   const [isSubmitted, setIsSubmitted] = useState(false)
 
   const handleSubmit = event => {
-    console.log('submitting')
     event.preventDefault()
+
     const formData = new FormData(event.target)
+
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: new URLSearchParams(formData).toString()
     })
       .then(response => {
-        if (!response.ok) throw new Error(`${response.status} (${response.statusText})`)
+        if (!response.ok)
+          throw new Error(`${response.status} (${response.statusText})`)
         setIsSubmitted(true)
       })
       .catch(error => alert(error))
@@ -23,24 +25,62 @@ export default function ContactFormContainer() {
 
   return (
     <ContactForm>
-      <ContactForm.Form netlify onSubmit={handleSubmit} style={{ display: isSubmitted ? 'none' : 'flex' }}>
-        <ContactForm.TextInput name="name" placeholder="Your Name" required maxLength="100" minLength="3">
-          Name<span style={{color: 'var(--clr-accent-primary)'}}>*</span>
+      <ContactForm.Form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+        style={{ display: isSubmitted ? 'none' : 'flex' }}
+      >
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="contact" />
+        <ContactForm.TextInput
+          name="name"
+          placeholder="Your Name"
+          required
+          maxLength="100"
+          minLength="3"
+        >
+          Name<span style={{ color: 'var(--clr-accent-primary)' }}>*</span>
         </ContactForm.TextInput>
-        <ContactForm.TextInput type="email" name="email" placeholder="Your Email" required maxLength="100" minLength="5">
-          Email<span style={{color: 'var(--clr-accent-primary)'}}>*</span>
+        <ContactForm.TextInput
+          type="email"
+          name="email"
+          placeholder="Your Email"
+          required
+          maxLength="100"
+          minLength="5"
+        >
+          Email<span style={{ color: 'var(--clr-accent-primary)' }}>*</span>
         </ContactForm.TextInput>
-        <ContactForm.TextInput name="subject" placeholder="Your Subject" maxLength="100">
+        <ContactForm.TextInput
+          name="subject"
+          placeholder="Your Subject"
+          maxLength="100"
+        >
           Subject
         </ContactForm.TextInput>
-        <ContactForm.TextInput textarea name="message" placeholder="Your Message" required maxLength="2000" minLength="10">
-          Message<span style={{color: 'var(--clr-accent-primary)'}}>*</span>
+        <ContactForm.TextInput
+          textarea
+          name="message"
+          placeholder="Your Message"
+          required
+          maxLength="2000"
+          minLength="10"
+        >
+          Message<span style={{ color: 'var(--clr-accent-primary)' }}>*</span>
         </ContactForm.TextInput>
         <ContactForm.SubmitButton>Send</ContactForm.SubmitButton>
       </ContactForm.Form>
-      <ContactForm.ConfirmationMessage style={{ display: isSubmitted ? 'unset' : 'none' }}>
+      <ContactForm.ConfirmationMessage
+        style={{ display: isSubmitted ? 'unset' : 'none' }}
+      >
         <p>Message sent!</p>
-        <p>Thank you for getting in touch, please expect a response at the email address you provided.</p>
+        <p>
+          Thank you for getting in touch, please expect a response at the email
+          address you provided.
+        </p>
       </ContactForm.ConfirmationMessage>
     </ContactForm>
   )
